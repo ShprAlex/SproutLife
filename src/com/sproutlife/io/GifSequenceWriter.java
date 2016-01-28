5620e348e82960411cc065843bb9e7c7d6d638c9
Alex Shapiro <shapiro.alex@gmail.com>
2016-01-28 11:47:21 -0500

    added copyright header to files

--- src/com/sproutlife/io/GifSequenceWriter.java
+++ src/com/sproutlife/io/GifSequenceWriter.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Alex Shapiro - github.com/shpralex
+ * This program and the accompanying materials
+ * are made available under the terms of the The MIT License (MIT)
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *******************************************************************************/
 package com.sproutlife.io;
 
 //
