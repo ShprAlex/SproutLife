1036f48c707dc14ce869caacc417aa1389c7fb41
Alex Shapiro <shapiro.alex@gmail.com>
2016-02-11 16:12:28 -0500

    Layout tweaks

--- src/com/sproutlife/panel/StatsPanel.java
+++ src/com/sproutlife/panel/StatsPanel.java
@@ -14,6 +14,7 @@ import java.awt.Insets;
 
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
+import javax.swing.JScrollPane;
 
 public class StatsPanel extends JPanel {
 	
@@ -34,19 +35,22 @@ public class StatsPanel extends JPanel {
 		
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[] {10, 100, 10};
-		gridBagLayout.rowHeights = new int[]{20, 0, 0, 15, 20, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{20, 0, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0};
-		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 0, 5);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 1;
+		gbc_scrollPane.gridy = 1;
+		add(scrollPane, gbc_scrollPane);
+		
 		statsTextPane = new JTextPane();
+		scrollPane.setViewportView(statsTextPane);
 		statsTextPane.setText("Start game to see statistics.");
-		GridBagConstraints gbc_statsTextPane = new GridBagConstraints();
-		gbc_statsTextPane.insets = new Insets(0, 0, 5, 5);
-		gbc_statsTextPane.fill = GridBagConstraints.BOTH;
-		gbc_statsTextPane.gridx = 1;
-		gbc_statsTextPane.gridy = 1;
-		add(statsTextPane, gbc_statsTextPane);
 
 	}
 
