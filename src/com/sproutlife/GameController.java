bf85b7be2fe67003f8ea6004f4ece776fb708425
Alex Shapiro <shapiro.alex@gmail.com>
2016-02-12 12:36:46 -0500

    Updated version revision #

--- src/com/sproutlife/GameController.java
+++ src/com/sproutlife/GameController.java
@@ -18,7 +18,7 @@ public class GameController {
     
     private static final int    appMajorVersion    = 1;
     private static final int    appMinorVersion    = 0;
-    private static final int    appRevision        = 6;
+    private static final int    appRevision        = 7;
     
     private GameModel gameModel;
     
