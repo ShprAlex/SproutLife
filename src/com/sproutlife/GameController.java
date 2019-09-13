67793871bf3c909f391a6881e46e15348db454f2
Alex Shapiro <shapiro.alex@gmail.com>
2019-09-13 14:14:33 -0400

    changed app version down to 0.12.1

--- src/com/sproutlife/GameController.java
+++ src/com/sproutlife/GameController.java
@@ -16,9 +16,9 @@ import com.sproutlife.panel.PanelController;
 
 public class GameController {
     
-    private static final int    appMajorVersion    = 1;
-    private static final int    appMinorVersion    = 1;
-    private static final int    appRevision        = 0;
+    private static final int    appMajorVersion    = 0;
+    private static final int    appMinorVersion    = 12;
+    private static final int    appRevision        = 1;
     
     private GameModel gameModel;
     
