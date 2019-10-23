0d30ecc0d5499bc5e7af04c6c86db10cd69ef20e
Alex Shapiro <shapiro.alex@gmail.com>
2019-10-22 21:23:29 -0400

    updated colors for black/white themes

--- src/com/sproutlife/renderer/colors/DefaultColorModel.java
+++ src/com/sproutlife/renderer/colors/DefaultColorModel.java
@@ -29,22 +29,42 @@ public class DefaultColorModel implements ColorModel {
 
     @Override
     public Color getCellColor(Organism o) {
-        int grayC = 100;
-        switch (o.getAttributes().kind) {
-            case 0: return new Color(255, grayC, grayC);
-            case 1: return new Color(grayC-10, 255, grayC-10);
-            case 2: return new Color(grayC, grayC ,255);
+        if (getBackgroundTheme() == BackgroundTheme.white) {
+            int grayC = 140;
+            switch (o.getAttributes().kind) {
+                case 0: return new Color(255, grayC, grayC);
+                case 1: return new Color(grayC-80, 255, grayC-80);
+                case 2: return new Color(grayC, grayC ,255);
+            }
+        }
+        else {
+            int grayC = 100;
+            switch (o.getAttributes().kind) {
+                case 0: return new Color(255, grayC, grayC);
+                case 1: return new Color(grayC-10, 255, grayC-10);
+                case 2: return new Color(grayC, grayC ,255);
+            }
         }
         return null;
     }
 
     @Override
     public Color getHeadColor(Organism o) {
-        int grayC = 186;
-        switch (o.getAttributes().kind) {
-            case 0: return new Color(255, grayC-10, grayC-10);
-            case 1: return new Color(grayC, 255, grayC);
-            case 2: return new Color(grayC, grayC, 255);
+        if (getBackgroundTheme() == BackgroundTheme.white) {
+            int grayC = 120;
+            switch (o.getAttributes().kind) {
+                case 0: return new Color(255, grayC, grayC);
+                case 1: return new Color(0, 230, 0);
+                case 2: return new Color(grayC, grayC, 255);
+            }
+        }
+        else {
+            int grayC = 80;
+            switch (o.getAttributes().kind) {
+                case 0: return new Color(255, grayC, grayC);
+                case 1: return new Color(0, 230, 0);
+                case 2: return new Color(grayC, grayC, 255);
+            }
         }
         return null; 
     }
